de6cc717e8d4463ca6bb14c13f743000d07fc744
Abhimanyu Narwal <anarwal.27@gmail.com>
2019-07-22 12:55:14 -0400

    update for tf v0.12

--- examples/simple-vpc/main.tf
+++ examples/simple-vpc/main.tf
@@ -5,14 +5,14 @@ provider "aws" {
 }
 
 module "networking_setup" {
-    source             = "../../module/"
-    namespace          = "ex"
-    stage              = "test"
-    attributes         = ["xyz"]
-    name               = "app"
-    delimiter          = "-"
-    cidr               = "10.0.0.0/16"
-    private_subnets    = ["10.0.32.0/20", "10.0.96.0/20"]
-    public_subnets     = ["10.0.0.0/19", "10.0.64.0/19"]
-    availability_zones = ["us-east-1a", "us-east-1b"]
+  source             = "../../module/"
+  namespace          = "ex"
+  stage              = "test"
+  attributes         = ["xyz"]
+  name               = "app"
+  delimiter          = "-"
+  cidr               = "10.0.0.0/16"
+  private_subnets    = ["10.0.32.0/20", "10.0.96.0/20"]
+  public_subnets     = ["10.0.0.0/19", "10.0.64.0/19"]
+  availability_zones = ["us-east-1a", "us-east-1b"]
 }
--- examples/simple-vpc/outputs.tf
+++ examples/simple-vpc/outputs.tf
@@ -1,34 +1,34 @@
 // The VPC ID
 output "vpc_id" {
-  value = "${module.networking_setup.vpc_id}"
+  value = module.networking_setup.vpc_id
 }
 
 // The VPC CIDR
 output "vpc_cidr_block" {
-  value = "${module.networking_setup.vpc_cidr_block}"
+  value = module.networking_setup.vpc_cidr_block
 }
 
 // A comma-separated list of public subnet IDs
 output "public_subnets" {
-  value = ["${module.networking_setup.public_subnets}"]
+  value = [module.networking_setup.public_subnets]
 }
 
 // A comma-separated list of private subnet IDs
 output "private_subnets" {
-  value = ["${module.networking_setup.private_subnets}"]
+  value = [module.networking_setup.private_subnets]
 }
 
 // The list of availability zones of the VPC.
 output "availability_zones" {
-  value = ["${module.networking_setup.availability_zones}"]
+  value = [module.networking_setup.availability_zones]
 }
 
 // Default Subnet ID for Database
 output "default_db_subnet_group" {
-  value = "${module.networking_setup.default_db_subnet_group}"
+  value = module.networking_setup.default_db_subnet_group
 }
 
 // The public route table ID.
 output "public_rtb_id" {
-  value = "${module.networking_setup.public_rtb_id}"
+  value = module.networking_setup.public_rtb_id
 }
