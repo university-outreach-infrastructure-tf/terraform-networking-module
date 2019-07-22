a0e9988a91e8db1ff840e7c67e370aa606dd959a
Abhimanyu Narwal <anarwal.27@gmail.com>
2019-07-22 11:22:04 -0400

    update to terraform 0.12

--- examples/simple-vpc/main.tf
+++ examples/simple-vpc/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 module "networking_setup" {
