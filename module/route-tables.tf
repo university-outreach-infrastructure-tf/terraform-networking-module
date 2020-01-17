cad42ed046de2e7af83fea9934fed76fa1df43cf
Abhimanyu Narwal <abhimanyunarwal@northwesternmutual.com>
2020-01-16 19:57:42 -0500

    fixing typo in module for route table resource name

--- module/route-tables.tf
+++ module/route-tables.tf
@@ -43,7 +43,7 @@ resource "aws_route_table" "private"{
 
 resource "aws_route" "private" {
   count                  = length(var.private_subnets)
-  route_table_id         = element(aws_route_table.public.*.id, count.index)
+  route_table_id         = element(aws_route_table.private.*.id, count.index)
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = element(aws_nat_gateway.main.*.id, count.index)
 }
