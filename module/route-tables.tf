a5a9a00a9eec8fa9ef38980712ddf28876c8a96b
Abhimanyu Narwal <abhimanyunarwal@northwesternmutual.com>
2020-01-21 13:51:40 -0500

    removing extra space in a file

--- module/route-tables.tf
+++ module/route-tables.tf
@@ -33,5 +33,5 @@ resource "aws_route_table_association" "private" {
   count          = length(var.private_subnets)
   subnet_id      = element(aws_subnet.private.*.id, count.index)
   route_table_id = element(aws_route_table.private.*.id, count.index)
-    depends_on   = [aws_subnet.private, aws_route_table.private]
+  depends_on     = [aws_subnet.private, aws_route_table.private]
 }
